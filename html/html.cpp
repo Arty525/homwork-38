23da316b8853db9f06a1e1a98a51e7f6dce5fca6
Andrey Egorov <89473462+Arty525@users.noreply.github.com>
2023-05-30 20:17:49 +0300

    Add files via upload

--- html/html.cpp
+++ html/html.cpp
@@ -1,1 +1,29 @@
+﻿#include <qapplication.h>
+#include <qresource.h>
+#include <QtGui/QtGui>
+#include <QWebEngineView>
+#include <QPlainTextEdit>
+#include <QHBoxLayout>
+#include <QTabWidget>
 
+int main(int argc, char** argv) {
+	QApplication app(argc, argv);
+	auto* window = new QWidget;
+	auto* htmlEdit = new QPlainTextEdit;
+	auto* webView = new QWebEngineView;
+	auto* hbox = new QHBoxLayout(window);
+
+	hbox->addWidget(htmlEdit);
+	hbox->addWidget(webView);
+	htmlEdit->setMinimumSize(500,500);
+	webView->setMinimumSize(500,500);
+
+	QObject::connect(htmlEdit, &QPlainTextEdit::textChanged, [webView, htmlEdit]() {
+		auto htmlCode = htmlEdit->toPlainText();
+		webView->setHtml(htmlCode);
+	});
+	window->show();
+	window->setFixedSize(1000, 500);
+
+	return app.exec();
+}
